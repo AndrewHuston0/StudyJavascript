ffbeb5bc7f20ff01bd642a72e7631d431702ab91
AndrewHuston0 <prototypezodiac@gmail.com>
2023-07-05 21:38:48 +0900

    day 2

--- momentum/script.js
+++ momentum/script.js
@@ -58,3 +58,43 @@ daysOfWeek.push("sun");
 console.log(daysOfWeek);
 
 // 변수의 효율적인 관리를 위해 array가 필요
+
+// Object
+
+const playerName = "hojun";
+const playerPoints = 121212;
+const playerHandsome = true;
+const playerFat = "not at all";
+// 위와 같은 경우 중복되는 player의 속성들이다. 결국 player.name, player.points 같은 경우가 더 직관적이고 잘 정돈되어 보일 것이다.
+
+// array의 경우 따로 아래처럼 표시해 주지 않는 이상 의미를 갖지 않는다
+// or PlayerList[0] == name .... 그리고 이는 매우 비효율적이다.
+const playerList = ["hojun", 121212, true, "not at all"];
+
+// 따라서 object를 사용하는데 이는 객체(entity)에서 다양한 하위 속성/특성(property)들을 가질 수 있다.
+const player = {
+    // object 안에서는 =를 사용하지 않는다. :를 사용한다.
+    name: "hojun",
+    points: 10,
+    fat: false,
+};
+console.log(player);
+console.log(player.name);
+player.name
+//console 또한 object라는 의미이며, 그 안의 어딘가에 log라는 것이 있다는 것.
+//list는 모든 값이 같은 의미를 갖음
+
+//표현하는 다른 방법으로 이것도 있음
+console.log(player["name"]);
+
+// 업데이트도 가능함
+console.log(player);
+player.name = "Kim";
+console.log(player);
+// 의문점 : const는 변경할 수 없는데 왜 변경?
+// 그러나 우리가 하는 건 const를 수정하는 게 아니라 constant 안에 내용을 바꾸는 것, object 자체를 수정하려고 하면 에러 발생
+
+// player = false;
+
+// other case
+player.lastName = "Kim";
